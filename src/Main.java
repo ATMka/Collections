bc399a9bdecb65089baaf5422644e86521ab2400
atmka <226408>
2019-07-25 17:13:25 +0400

    Создание проекта

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import Task001.Demo;
+
 public class Main {
     public static void main(String[] args) {
-
+        Demo demo = new Demo();
+        demo.go();
     }
 }
--- src/Task001/CountRepeatWords.java
+++ src/Task001/CountRepeatWords.java
@@ -1,5 +1,21 @@
 package Task001;
 
-public class CountRepeatWords {
+import java.util.HashMap;
+import java.util.Map;
 
+public class CountRepeatWords {
+    private Map<Long, String> textOnly = new HashMap<>();
+    private Long hashCode = 0L;
+    public Map<Long, String> textToTextOnly (String text){
+        for (String word: text.split("[^a-zA-Zа-яёА-ЯЁ]+")
+             ) {
+            textOnly.put(hashCode,word);
+            hashCode++;
+        }
+        return textOnly;
+    }
+
+    public Map<Long, String> getTextOnly() {
+        return textOnly;
+    }
 }
